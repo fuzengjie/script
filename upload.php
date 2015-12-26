069ff23a18b198c84b9445729f7da6dbdb7e942b
fuzengjie <linux_SVIP@163.com>
2015-12-26 17:27:37 +0800

    ad

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@ $YUM_DIR = "/data/repo";
 $SCRIPT_DIR="/data/script/";
 if ($_FILES['file']['name'] != '') {  
     if ($_FILES['file']['error'] > 0) {  
-        echo "upload false<br/>";  
+        echo "upload false";  
     }  
     else {  
 $name=$_FILES['file']["name"];//上传文件的文件名 
@@ -24,27 +24,22 @@ $prefix=substr($name , 0 , 6);
          elseif ($suffix == 'rpm' && $prefix != 'beyond') {
                 $DST_DIR=($YUM_DIR."/extend/Packages/");
                 }
-        elseif ($suffix == 'sh' || $suffix == 'py' || $suffix == 'php') {
+         elseif ($suffix == 'sh' || $suffix == 'py' || $suffix == 'php') {
                 $DST_DIR=$SCRIPT_DIR;
-        }
+        	}
         else { $DST_DIR=$UP_DIR;
-	 }
-
-echo "=================================<br/>"; 
-echo "Upload FileName: ".$name."<br/>"; 
-echo "Upload FileType: ".$type."<br/>"; 
-echo "upload FileSize: ".$size."<br/>"; 
-
+	 	}
         if (move_uploaded_file($_FILES['file']['tmp_name'], $DST_DIR.$_FILES['file']['name'])) {  
-	    echo "Upload  FileStatus: success<br/>";  
-	    echo "Access URL:".$url."<br/>";
-       }  else {  
-            echo "Upload FileStatus: false<br/>";  
-        }  
+	    	$status="success";
+       	  }
+	  else {  
+		$status="false";
+		$url='none';
+           }
+	echo json_encode(array('FileURL'=> $url,'status'=>$status,'FileSize'=> $size,'FileType'=> $type,'FileName'=>$name));
     } 
-echo "==================================<br/>";
 }
 else {  
-	    echo "please choose a file<br/>";  
+	    echo "please choose a file";  
 }
 ?>
