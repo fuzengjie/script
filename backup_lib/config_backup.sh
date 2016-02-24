cf3fd65a51c316b643e9348d58ea875f5539825b
fuzengjie <linux_SVIP@163.com>
2016-02-24 22:42:31 +0800

    1

--- backup_lib/config_backup.sh
+++ backup_lib/config_backup.sh
@@ -16,8 +16,11 @@ USER=backup\n
 PASSWORD_FILE=/data/script/backup_lib/passwd\n
 LOCAL_FILE=$Path\n
 DATE=\`date +%Y%m%d%H%M\`\n
+REMOTE_NAME=\`echo \$LOCAL_FILE | awk -F \"/\" '{print \$NF}'\`\n
 LOG=./backup.log\n
-/usr/bin/rsync -avzP ./\${LOCAL_FILE} backup@\${BACKUP_SERVER}::\$MODULE/\$HOSTNAME/\$DATE/ --password-file=\${PASSWORD_FILE} >> \$LOG\n
+/usr/bin/rsync -avzP \${LOCAL_FILE} backup@\${BACKUP_SERVER}::\$MODULE/\$HOSTNAME/\${REMOTE_NAME}_\$DATE/ --password-file=\${PASSWORD_FILE} >> \$LOG\n
 "
 
 echo -e  $info > bak_${SCRIPT_NAME}.sh
+
+echo "*/$INTERVAL * * * * root /bin/bash /data/script/backup_lib/bak_${SCRIPT_NAME}.sh > /dev/null 2>&1"  >> /etc/cron.d/backup
